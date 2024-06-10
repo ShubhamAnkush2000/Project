1c579ec3611fecf1ca52cb23d993cb2ff8dd8267
SHUBHAM RAJESH ANKUSH <112404733+ShubhamAnkush2000@users.noreply.github.com>
2024-06-10 12:42:53 +0530

    Add files via upload

---
+++ Atm.cpp
@@ -0,0 +1,154 @@
+// C++ program to implement the ATM
+// Management System
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+using namespace std;
+class Bank {
+ 
+    // Private variables used inside class
+private:
+    string name;
+    long long accnumber;
+    char type[10];
+    long long amount ;
+    long long tot ;
+    
+ 
+    // Public variables
+public:
+    // Function to set the person's data
+     char Fname[20];
+     int fd = 0 , Length = 0;
+    
+     char Data[100];
+
+    void setvalue()
+    {
+        cout << "Enter name\n";
+        cin.ignore();
+ 
+        // To use space in string
+        getline(cin, name);
+ 
+        cout << "Enter Account number\n";
+        cin >> accnumber;
+        cout << "Enter Account type\n";
+        cin >> type;
+        cout << "Enter Balance\n";
+        cin >> tot;
+       
+    }
+ 
+    // Function to display the required data
+    void showdata()
+    {
+        cout << "Name:" << name << endl;
+        cout << "Account No:" << accnumber << endl;
+        cout << "Account type:" << type << endl;
+        cout << "Balance:" << tot << endl;
+    }
+ 
+    // Function to deposit the amount in ATM
+    void deposit()
+    {
+        cout << "\nEnter amount to be Deposited\n";
+        cin >> amount;
+    }
+ 
+    // Function to show the balance amount
+    void showbal()
+    {
+        tot = tot + amount;
+        cout << "\nTotal balance is: " << tot;
+    }
+ 
+    // Function to withdraw the amount in ATM
+    void withdrawl()
+    {
+        int a, avai_balance;
+        cout << "Enter amount to withdraw\n";
+        cin >> a;
+        avai_balance = tot - a;
+        cout << "Available Balance is" << avai_balance;
+    }
+    int receipt()
+    {  
+    printf("Receipt Number : \n");
+    scanf("%s",Fname);
+
+    fd = open(Fname,O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+    while((Length = read(fd,Data,sizeof(Data)))!= 0)
+    {
+        write(1,Data,Length);
+
+    }
+    close(fd);
+    
+    }
+};
+ 
+// Driver Code
+int main()
+{
+    // Object of class
+    Bank b;
+ 
+    int choice;
+ 
+    // Infinite while loop to choose
+    // options everytime
+    while (1) {
+        cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~"
+             << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+             << "~~~WELCOME~~~~~~~~~~~~~~~~~~"
+             << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+             << "~~~~~~~~~\n\n";
+        cout << "Enter Your Choice\n";
+        cout << "\t1. Enter name, Account "
+             << "number, Account type\n";
+        cout << "\t2. Balance Enquiry\n";
+        cout << "\t3. Deposit Money\n";
+        cout << "\t4. Show Total balance\n";
+        cout << "\t5. Withdraw Money\n";
+        cout <<"\t6. receipt\n ";
+        cout << "\t7. Cancel\n";
+        cin >> choice;
+ 
+        // Choices to select from`
+        switch (choice) {
+        case 1:
+            b.setvalue();
+            break;
+        case 2:
+            b.showdata();
+            break;
+        case 3:
+            b.deposit();
+            break;
+        case 4:
+            b.showbal();
+            break;
+        case 5:
+            b.withdrawl();
+            break;
+        case 6:
+            b.receipt();
+            break;
+        case 7:
+            exit(1);
+            break;
+        default:
+            cout << "\nInvalid choice\n";
+        }
+    }
+}
+ 
